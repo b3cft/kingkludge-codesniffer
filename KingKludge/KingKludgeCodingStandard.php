b041e33947ecb80344dea0cb0176dd29b878f3dc
Andrew 'Bob' Brockhurst <andy@brockhurst.com>
2009-02-05 22:07:06 +0000

    empty standard complete

--- KingKludge/KingKludgeCodingStandard.php
+++ KingKludge/KingKludgeCodingStandard.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * KingKludge Coding Standard.
+ * KingKludge Coding Standard
  *
  * PHP version 5
  *
  * @category  PHP
  * @package   PHP_CodeSniffer_Yahoo
  * @author    Andy 'Bob' Brockhurst <andy@brockhurst.com>
- * @version   $Id: YahooCodingStandard.php,v 1.5 2008/12/18 14:53:39 abrock Exp $
+ * @version   $Id: $
  */
 
 if (class_exists('PHP_CodeSniffer_Standards_CodingStandard', true) === false) {
-    throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_Standards_CodingStandard not found');
+    throw new PHP_CodeSniffer_Exception(                     'Class PHP_CodeSniffer_Standards_CodingStandard not found'                                       );
 }
 
 /**
@@ -23,44 +23,30 @@ if (class_exists('PHP_CodeSniffer_Standards_CodingStandard', true) === false) {
  */
 class PHP_CodeSniffer_Standards_KingKludge_KingKludgeCodingStandard extends PHP_CodeSniffer_Standards_CodingStandard
 {
-
-
     /**
      * Return a list of external sniffs to include with this standard.
      *
-     * The MySource standard is an extension of the Squiz standard, with
-     * specific tests for the MySource CMS, so include the whole Squiz standard.
+     * The standard can include the whole standards or individual Sniffs.
      *
      * @return array
      */
     public function getIncludedSniffs()
     {
-        return array(
-                'Generic/Sniffs/PHP/DisallowShortOpenTagSniff.php',
-                'Generic/Sniffs/PHP/NoSilencedErrorsSniff.php',
-                'PEAR/Sniffs/Files/IncludingFileSniff.php',
-                'PEAR/Sniffs/Files/LineEndingsSniff.php',
-                'Squiz/Sniffs/Operators/ValidLogicalOperatorsSniff.php',
-                'Squiz/Sniffs/PHP/EvalSniff.php',
-               );
+        return array();
 
     }//end getIncludedSniffs()
 
     /**
      * Return a list of external sniffs to exclude from this standard.
      *
-     * The PHP_CodeSniffer standard combines the PEAR and Squiz standards
-     * but removes some sniffs from the Squiz standard that clash with
-     * those in the PEAR standard.
+     * Including a whole standards above, individual Sniffs can then be removed here.
      *
      * @return array
      */
     public function getExcludedSniffs()
     {
-        return array(
-                    );
+        return array();
 
     }//end getExcludedSniffs()
-
-}//end class
+}
 ?>
